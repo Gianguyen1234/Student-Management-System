422fb906989083119b65d8d2abebaef6a53a60af
Thao Nguyen <nguyenluthao252@gmail.com>
2023-10-29 23:12:31 +0700

    fix done

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "de"],
+    locales: ["en", "vn"],
   },
   localePath: path.resolve("./public/locales"),
 };
